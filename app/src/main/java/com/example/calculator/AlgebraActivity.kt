9bfb0fb8db564ce06d28921ad32fdf813a4cd2f9
mehulbest <157376178+mehulbest@users.noreply.github.com>
2024-07-02 14:58:58 +0100

    Update AlgebraActivity.kt

--- app/src/main/java/com/example/calculator/AlgebraActivity.kt
+++ app/src/main/java/com/example/calculator/AlgebraActivity.kt
@@ -8,7 +8,7 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import net.objecthunter.exp4j.ExpressionBuildergit
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class AlgebraActivity : AppCompatActivity(), View.OnClickListener {
 
